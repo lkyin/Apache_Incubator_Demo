2195a6948a2f0a62d0abe5c9407200bc544a1bb8
lkyin <lkyin@ucdavis.edu>
2020-06-12 10:58:14 -0700

    using extension of data

--- code.js
+++ code.js
@@ -1,42 +1,11 @@
 
 // values for all forces
-forceProperties = {
-    center: {
-        x: 0.5,
-        y: 0.5
-    },
-    charge: {
-        enabled: true,
-        strength: -30,
-        distanceMin: 1,
-        distanceMax: 2000
-    },
-    collide: {
-        enabled: true,
-        strength: .7,
-        iterations: 1,
-        radius: 5
-    },
-    forceX: {
-        enabled: false,
-        strength: .1,
-        x: .5
-    },
-    forceY: {
-        enabled: false,
-        strength: .1,
-        y: .5
-    },
-    link: {
-        enabled: true,
-        distance: 30,
-        iterations: 1
-    },
+tProperties = {
+
     // load the selected data
     selected_data: {
         project: 49,
-        month: 1,
-        ntype: 'email'
+        month: 1
     }
 }
 
@@ -54,8 +23,8 @@ function readTextFile(file){
     return allText;
 }
 
-// `directed_network/p${forceProperties.selected_data.project}m${forceProperties.selected_data.month}_commit.json`
-var data = eval(readTextFile("p49m1_commit.json"))
+// 
+var data = eval(readTextFile(`directed_network/p${tProperties.selected_data.project}m${tProperties.selected_data.month}_commit.json`))
 
 var color ={Elite:"#3366CC", Grand:"#DC3912",  Lite:"#FF9900", Medium:"#109618", Plus:"#990099", Small:"#0099C6"};
 var svg_t = d3.select("body").append("svg").attr("width", 960).attr("height", 800);
